819f9e5f0b30ae4ce40515f3ae4557435057aac5
Radha Naik <21radhanaik999@gmail.com>
2026-07-15 03:26:30 -0700

    variables and datatypes

---
+++ Day1/DataTypes.java
@@ -0,0 +1,21 @@
+class DataTypes{
+    public static void main(String a[]){
+        //primitive datatypes
+        int n=9;//4 bytes=32bits 
+
+       // byte b=128;
+       byte b=127;//1 byte=8bits
+       //short s=32768;
+       short s=900;//2byte=16bits
+       long l=550;//8byte=64bits
+        //float f1=5.5;
+       float f=5.5f;//float is 4byte we cannot assign 5.5 bigger value into the float which is 8byte
+       double d1=5.5;//8byte=32bits
+       //char ch="A";
+       char ch='A';//2byte
+       boolean b1=true;//no fixed size it only store true or false
+       
+
+
+    }
+}
---
+++ Day1/Variables.java
@@ -0,0 +1,13 @@
+class Variables{
+    public static void main(String a[]){
+        System.out.println(5+4);
+        int n1=5;
+        int n2=3;
+        System.out.println(n1+n2);
+        int num1=6;
+        int num2=4;
+        int re=num1+num2;
+    
+        System.out.println("result:"+re);
+    }
+}
